b51f35d4894e3067e0c7362247f13fb00a00e617
Lindsay Kuperman <LMK22E@FSU.EDU>
2024-11-05 19:39:56 -0500

    fish is made!

--- sketch.js
+++ sketch.js
@@ -1,53 +1,32 @@
 let fish;
-let sharks = [];
-let sharkCount = 5;
 
 function setup() {
-  createCanvas(800, 600);
-  
-  // Create the fish (player)
-  fish = createSprite(width / 2, height / 2, 40, 20);
-  fish.shapeColor = color(0, 100, 250);
+  new Canvas(500, 500);
+  displayMode('centered');
 
-  // Create sharks (enemies)
-  for (let i = 0; i < sharkCount; i++) {
-    let shark = createSprite(random(width), random(height), 60, 30);
-    shark.shapeColor = color(150, 0, 0);
-    shark.setSpeed(random(1, 3), random(360)); // Random movement
-    sharks.push(shark);
-  }
+  // Create the fish sprite
+  fish = new Sprite();
+  fish.diameter = 50;
+
+  // Define custom drawing for the fish sprite
+  fish.draw = function() {
+    fill(0, 150, 255); // Body color
+    ellipse(0, 0, 50, 20); // Fish body
+    fill(0, 100, 200);
+    triangle(-25, 0, -40, -10, -40, 10); // Tail fin
+    fill(255);
+    ellipse(15, -5, 5, 5); // Eye
+    fill(0);
+    ellipse(16, -5, 2, 2); // Pupil
+  };
 }
 
 function draw() {
-  background(0, 150, 255); // Ocean blue background
+  background('skyblue');
 
-  // Fish movement
-  if (keyIsDown(LEFT_ARROW)) fish.position.x -= 5;
-  if (keyIsDown(RIGHT_ARROW)) fish.position.x += 5;
-  if (keyIsDown(UP_ARROW)) fish.position.y -= 5;
-  if (keyIsDown(DOWN_ARROW)) fish.position.y += 5;
-
-  // Loop over sharks to check for collisions and movement
-  for (let shark of sharks) {
-    if (fish.overlap(shark)) {
-      gameOver();
-      break;
-    }
-
-    // Make sharks wrap around screen
-    if (shark.position.x > width) shark.position.x = 0;
-    if (shark.position.x < 0) shark.position.x = width;
-    if (shark.position.y > height) shark.position.y = 0;
-    if (shark.position.y < 0) shark.position.y = height;
+  // Make the fish move towards the mouse when clicked
+  if (mouse.presses()) {
+    fish.speed = 10;
+    fish.moveTo(mouse);
   }
-
-  drawSprites();
-}
-
-function gameOver() {
-  noLoop(); // Stop the game
-  textSize(32);
-  fill(255);
-  textAlign(CENTER, CENTER);
-  text("Game Over!", width / 2, height / 2);
 }
